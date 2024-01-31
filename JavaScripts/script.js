f1d7f891e893f564fbbd9875c14718f457034f4b
Mohammad Ali <113818908+mohammadali-150236@users.noreply.github.com>
2024-01-31 20:39:21 +0600

    Add files via upload
    Version 3.0

---
+++ JavaScripts/script.js
@@ -0,0 +1,42 @@
+function aA(){
+window.location.href = "https://eticket.railway.gov.bd";
+            }
+function b(){
+window.location.href = "../HTMLs/bus.html"
+            }
+function c(){
+window.location.href = "https://www.biman-airlines.com/"
+            }
+function d(){
+window.location.href = "https://www.bdtickets.com/launch"
+            }
+function e(){
+window.location.href = "../HTMLs/ads.html"
+            }
+function f(){
+window.location.href = "../HTMLs/help.html"
+            }
+function i() {
+    window.location.href = "../HTMLs/exploreBD.html"
+}
+function j() {
+window.location.href = "../HTMLs/help&support.html"    
+}
+
+function about() {
+    window.location.href = "../HTMLs/about.html"
+}
+// script.js
+
+function toggleMenu() {
+    var menu = document.getElementById('main-menu');
+    menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
+    // Toggle the 'active' class for smooth opening and closing
+    menu.classList.toggle('active');
+
+    // Check if the menu is open or closed
+    var isOpen = menu.classList.contains('active');
+
+    // Set the max-height based on whether the menu is open or closed
+    menu.style.maxHeight = isOpen ? menu.scrollHeight + 'px' : '0';
+}
---
+++ JavaScripts/scriptforbus.js
@@ -0,0 +1,121 @@
+
+
+function hanif() {
+    window.location.href = "https://www.hanif-enterprise.com/";
+}
+
+function greenLine() {
+    window.location.href = "https://www.greenlinebd.com/";
+}
+
+function shyamoli() {
+    window.location.href = "https://www.shyamoliparibahan-bd.com/";
+}
+
+function eagle() {
+    window.location.href = "https://www.eagleparibahan.com/";
+}
+
+function falguni() {
+    window.location.href = "https://www.falgunimodhumotibd.com/";
+}
+
+function robiExpress() {
+    window.location.href = "https://saintmartinhyundai.net/";
+}
+
+function brtc() {
+    window.location.href = "https://ticketing-brtc.gov.bd/";
+}
+
+function b() {
+    window.location.href = "index.html";
+}
+
+function imperial() {
+    window.location.href = "https://www.imperialexpressbd.com/en_US/";
+}
+
+function shohagh() {
+    window.location.href = "https://www.shohagh.com/";
+}
+
+function showShohagh(){
+    var sideShohagh = document.querySelector(".shohagh")
+    sideShohagh.style.right = '0'
+}
+
+function hide(){
+    var sideShohagh = document.querySelector(".shohagh")
+    sideShohagh.style.right = '-100%'
+}
+
+function showHanif(){
+    var sideHanif = document.querySelector(".hanif")
+    sideHanif.style.right = '0'
+}
+
+function hideHanif(){
+    var sideHanif = document.querySelector(".hanif")
+    sideHanif.style.right = '-100%'
+}
+
+function showGreen() {
+    var sideGreen = document.querySelector(".green")
+    sideGreen.style.right = '0'
+}
+
+function hideGreen() {
+    var sideGreen = document.querySelector(".green")
+    sideGreen.style.right = '-100%'
+}
+
+function showShyamoli() {
+    var sideS = document.querySelector('.shyamoli');
+    sideS.style.right = '0';
+}
+
+function hideShyamoli() {
+    var hideS = document.querySelector('.shyamoli');
+    hideS.style.right = '-100%';
+}
+
+function showEagle() {
+    var sideE = document.querySelector('.eagle')
+    sideE.style.right = '0'
+}
+
+function hideEagle() {
+    var hideE = document.querySelector('.eagle')
+    hideE.style.right = '-100%'
+}
+
+function showImperial() {
+    var sideI = document.querySelector('.imperial')
+    sideI.style.right = '0'
+}
+
+function hideImperial() {
+    var hideI = document.querySelector('.imperial')
+    hideI.style.right = '-100%'
+}
+
+function showRobi() {
+    var sideR = document.querySelector('.robi')
+    sideR.style.right = '0'
+}
+
+function hideRobi() {
+    var hideR = document.querySelector('.robi')
+    hideR.style.right = '-100%'
+}
+
+function showFalguni() {
+    var sideF = document.querySelector('.falguni')
+    sideF.style.right = '0'
+}
+
+function hideFalguni() {
+    var hideF = document.querySelector('.falguni')
+    hideF.style.right = '-100%'
+}
