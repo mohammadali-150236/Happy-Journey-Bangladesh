cbe6811349e95997761c9b845b9ebdb159038449
Mohammad Ali <113818908+mohammadali-150236@users.noreply.github.com>
2024-02-07 20:26:00 +0600

    Add files via upload

---
+++ JavaScripts/scriptforabout.js
@@ -0,0 +1,21 @@
+function g(){
+window.location.href = "https://youtube.com/@md.alisproject198"
+            }
+function h(){
+window.location.href = "https://www.facebook.com/profile.php?id=100077457679406"
+            }
+function i(){
+window.location.href = "https://www.instagram.com/mohammadali.150236/"
+            }
+function j(){
+window.location.href = "https://github.com/mohammadali-150236"
+            }
+function k(){
+window.location.href = "https://wa.me/+8801883081445"
+            }
+function l(){
+window.location.href = "mailto:mohammadali.150236@gmail.com"
+            }
+function b(){
+    window.location.href = "index.html"
+}
